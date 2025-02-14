712dd267528b94ca8b121584f579ea5f12a742a7
derRafl <rafaelkudla147@outlook.de>
2025-02-14 15:29:00 +0100

    14.02.2025

--- calc/binom.go
+++ calc/binom.go
@@ -3,6 +3,26 @@ package calc
 // BinomialCoefficient erwartet zwei Zahlen n und k und liefert den
 // Binomialkoeffizienten "n über k".
 func BinomialCoefficient(n, k int) int {
-	// TODO
-	return 0
+	if n < 1 || k < 1 || (n == 1 && k == 1) || n == k {
+		return 1
+	}
+
+	return BinomialCoefficient(n-1, k) + BinomialCoefficient(n-1, k-1)
 }
+
+/**
+	func BinomialCoefficient(n, k int) int {
+
+		res := (fak(n)) / (fak(k) * fak(n-k))
+
+		return res
+	}
+
+	func fak(n int) int {
+		if n == 0 {
+			return 1
+		}
+
+		return (n * fak(n-1))
+	}
+**/
--- calc/mult.go
+++ calc/mult.go
@@ -2,8 +2,10 @@ package calc
 
 // Liefert das Produkt von x und y.
 func Mult(x, y int) int {
-	result := 1
 
-	// TODO
-	return result
+	if x == 0 || y == 0 {
+		return 0
+	}
+
+	return x + Mult(x, y-1)
 }
